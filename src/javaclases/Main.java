87fd4a2d1d5da015d0e364c67829c5bf7db6f104
pablo <p.rodriguez103@usp.ceu.es>
2023-02-21 18:47:03 +0100

    asasa

--- src/javaclases/Main.java
+++ src/javaclases/Main.java
@@ -1,7 +1,20 @@
 package javaclases;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-	
+	//List<String> symptoms= new ArrayList<String>();
+	//symptoms=("Shortness_of_breath_or_difficulty_breathing", "Rapid_breathing_or_breathing_too_fast",
+	//		"Wheezing", "Cyanosis", "Headache","Confusion_or_Disorientation", "Irregular_heartbeat", "Chest_pain",
+	//		"Chest_pain_that_gets_worse_when_you_breathe_deeply_or_cough", "Fatigue_or_Weakness", "Dizzines",
+	//		"Fainting_or_lost_of_consciousness"," Excessive_daytime_sleepiness", "Difficulty_concentrating",
+	//		"Irritability", "Depression","Anxiety"," Restless_sleep"," Frequent_awakenings_at_night"," Insomia", 
+			//		"Dry_mouth_or_sore_throat","Chest_oppression_or_and_discomfort"," Muscle_or_body_pain", "Cough", 
+	//		"Cough_with_blood"," Cough_that_gets_worse_over_time","Difficulty_performing_physical_activities",
+	//	"Loss_of_apetite_or_weight_loss", "Hoarseness", "Trouble_swallowing","Fever_or_chills",
+			//		"Loss_of_taste_or_smell", "Congestion_or_running_nose", "Nausea_or_vomiting", "Diarrhea",
+	//		"Inflammation_of_the_respiratory_tract", "Dyspnea", "Sweating", "Mucus", "Expectoration", 
+	//		"Stridor", "Hypoxemia", "Tachycardia","Hypertemia", "Loss_of_negative_pressure", "Sneezing", 
+	//		"Chronic_cough");
 }
